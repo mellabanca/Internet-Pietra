03fbb9632d33c756b8f82c4c771276443fc286a1
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-02-08 15:15:53 -0300

    08/02/2022 DESORGANIZADO

--- sketch.js
+++ sketch.js
@@ -1,29 +1,29 @@
 var internet, internetmovel;
-var solo, soloimagem;
+var areia, areiamovedica;
 function preload(){
 internetmovel = loadAnimation("trex1.png","trex3.png","trex4.png");
-soloimagem = loadImage("ground2.png");
+areiamovedica = loadImage("ground2.png");
 }
 function setup(){
 createCanvas(600,200)
 internet = createSprite(50, 160, 20, 50);
 internet.addAnimation("correndo", internetmovel);
 internet.scale = 0.5;
-solo = createSprite(200,180,400,20)
-solo.addImage("chão",soloimagem);
-solo.x = solo.width/2;
+areia = createSprite(200,180,400,20)
+areia.addImage("chão",areiamovedica);
+areia.x = areia.width/2;
 borda = createEdgeSprites();
 }
 function draw(){
 background("white");
-solo.velocityX = -2;
-if(solo.x < 0){
-solo.x = solo.width/2;
+areia.velocityX = -2;
+if(areia.x < 0){
+areia.x = areia.width/2;
 }
 if(keyDown("space")){
 internet.velocityY = -10;
 }
 internet.velocityY += 1;
-internet.collide(solo);
+internet.collide(areia);
 drawSprites();
 }
